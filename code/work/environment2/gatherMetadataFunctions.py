60a5f9d80f1305d2f63d064c517c48e42557fcdd
rubenros1795 <rubenros@live.nl>
2020-02-17 09:09:16 +0100

    updates for dates exploration and full text scraping

--- code/work/environment2/gatherMetadataFunctions.py
+++ code/work/environment2/gatherMetadataFunctions.py
@@ -11,6 +11,7 @@ import json
 import os
 from gatherImagesFunctions import *
 from htmldate import find_date
+from newspaper import Article
 
 base_path = os.getcwd()
 
@@ -37,12 +38,14 @@ def gatherImagessUrls(name, n_folders) :
     return all_url
 
 def gatherPagesUrls(name, n_folders) :
+    print("Initating gatherPagesUrls")
     base_path = os.getcwd()
     all_url = []
     for n in range(1,n_folders+1):
         folder_path = name + str(n)
         folder_path = os.path.join(base_path, folder_path)
         print(folder_path)
+
         if os.path.isdir(folder_path):
             list_json = [j for j in os.listdir(folder_path) if ".json" in j]
         if os.path.isdir(os.path.join(folder_path,"img")):
@@ -56,20 +59,66 @@ def gatherPagesUrls(name, n_folders) :
             except KeyError:
                 print('corrupted json file, probably an 400 Error!')
                 continue
+    print('finished gatherPagesUrls')
+    return all_url
+
+def gatherPagesUrlsFolder(folder_path) :
+    if os.path.isdir(folder_path):
+        list_json = [j for j in os.listdir(folder_path) if ".json" in j]
+    if os.path.isdir(os.path.join(folder_path,"img")):
+        list_imgs = [i for i in os.listdir(os.path.join(folder_path,"img"))]
+
+    all_url = []
+    for js in list_json:
+        json_data = loadJson(os.path.join(folder_path,js))
+        try:
+            temp_url = getPages(json_data)
+            all_url = all_url + temp_url
+        except KeyError:
+            print('corrupted json file, probably an 400 Error!')
+            continue
+    print('finished gatherPagesUrlsFolder')
     return all_url
 
 def gatherDates(list_urls):
     url_d = dict()
     for i in list_urls:
         #print(i)
-        d = find_date(i)
-        url_d.update({i:d})
+        try:
+            d = find_date(i)
+            url_d.update({i:d})
+        except Exception as e:
+            print(e)
+            continue
     return url_d
 
-l = gatherPagesUrls("image_ks_",2)
+def gatherSingleDate(url):
+    try:
+        d = find_date(url)
 
-l = l[0:40]
-d = gatherDates(l)
+    except Exception as e:
+        print(e)
+        return
 
-for x,z in d.items():
-    print(z)
+    return {url:d}
+
+def gatherFullText(url):
+    article = Article(url)
+    article.download()
+    article.parse()
+    txt = article.text
+    txt = txt.replace('\n\n', '\n')
+    return txt
+
+def gatherHtml(url):
+    fn = regexz.sub(r'\W+', '', url)
+    fn = fn + ".html"
+
+    try:
+        #urllib.request.urlretrieve(url, fn)
+        with open(fn, "w") as file:
+            file.write(str(soup))
+    except Exception as e:
+        #except (HTTPError, URLError, TimeoutError,IncompleteRead, ConnectionError) as e:
+        print(e)
+        return 0
