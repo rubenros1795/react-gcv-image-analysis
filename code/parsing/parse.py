27ad00e8245d8e3459203b93be7a6ae9dafbe0fa
rubenros1795 <rubenros@live.nl>
2020-06-18 11:19:49 +0200

    Update parse.py

--- code/parsing/parse.py
+++ code/parsing/parse.py
@@ -7,26 +7,20 @@ from concurrent.futures import ThreadPoolExecutor
 from bs4 import BeautifulSoup
 import json
 
-
-'''
-Step 1: Scrape Pages
-'''
-
-
 base_path = "/path/to/photos/"
 photo = "2"
 
 # # Parse Texts
-# Parse.Texts(base_path,"2")
-#
+Parse.Texts(base_path,"2")
+
 # # Parse Languages
-# Parse.Languages(base_path,"2")
-#
+Parse.Languages(base_path,"2")
+
 # # Parse Dates
-# Parse.Dates(base_path,"2")
-#
+Parse.Dates(base_path,"2")
+
 # # Parse Entities
-# Parse.Entities(base_path,"2")
+Parse.Entities(base_path,"2")
 
 # Parse ContextImages
 Parse.ContextImages(base_path,"2")
