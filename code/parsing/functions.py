8414ac4bfe9c5e34afe1059059c0fae5009188b4
rubenros1795 <rubenros@live.nl>
2020-06-18 11:25:42 +0200

    Update functions.py

--- code/parsing/functions.py
+++ code/parsing/functions.py
@@ -28,6 +28,200 @@ identifier = LanguageIdentifier.from_modelstring(model, norm_probs=True)
 
 nlp = spacy.load('en_core_web_sm')
 
+from boilerpipe.extract import Extractor
+from nltk.tokenize import word_tokenize
+from http.client import IncompleteRead
+from htmldate import find_date
+from bs4 import BeautifulSoup
+import concurrent.futures
+from tqdm import tqdm
+from PIL import Image
+import numpy as np
+import urllib.request
+import pandas as pd
+import re as regexz
+import codecs
+import datetime
+import requests
+import os.path
+import string
+import random
+import shutil
+import json
+import math
+import time
+import nltk
+import glob
+import uuid
+import csv
+import io
+
+import langid
+from langid.langid import LanguageIdentifier, model
+identifier = LanguageIdentifier.from_modelstring(model, norm_probs=True)
+
+"""
+Helper functions for scraping.
+Json() handles importing and querying the jsons
+Img() handles the Scraping
+Duplicates() removes images that are similar in width, height and color (rgb)
+"""
+
+class Json():
+    def __init__(self, filename, list_json):
+        self.filename = filename
+        self.json_object = json_object
+        self.list_json = list_json
+
+    def load(filename):
+        with open(filename) as json_file:
+            data = json.load(json_file)
+            return data
+
+    def extract_images(filename):
+
+        json_object = Json.load(filename)
+
+        list_url = []
+
+        for c,i in enumerate(json_object['responses'][0]['webDetection']['pagesWithMatchingImages']):
+            if 'partialMatchingImages' in i.keys():
+
+                for c2,x in enumerate(i['partialMatchingImages']):
+                    for u in list(dict(x).values()):
+                        list_url.append(u)
+
+            if 'fullMatchingImages' in i.keys():
+
+                for c2,x in enumerate(i['fullMatchingImages']):
+                    for u in list(dict(x).values()):
+                        list_url.append(u)
+        return list_url
+
+    def extract_image_folder(list_json):
+        all_url = []
+        for js in list_json:
+            try:
+                temp_url = Json.extract_images(js)
+                all_url = all_url + temp_url
+            except KeyError:
+                print('wrong .json input, there are probably no images in this .json')
+                continue
+        return list(set(all_url))
+
+    def extract_pages(filename):
+        json_object = Json.load(filename)
+
+        list_url = []
+
+        for c,i in enumerate(json_object['responses'][0]['webDetection']['pagesWithMatchingImages']):
+            list_url.append(i['url'])
+        return list_url
+
+class Img():
+
+    def __init__(url,list_url):
+        self.url = url
+        self.list_url = list_url
+
+    def Scraper(url):
+        fn = uuid.uuid1()
+
+        if "png" in url:
+            fn = str(fn) + ".png"
+        elif "jpg" in url:
+            fn = str(fn) + ".jpg"
+        elif "Jpeg" in url:
+            fn = str(fn) + ".jpg"
+        elif "jpeg" in url:
+            fn = str(fn) + ".jpg"
+        elif "JPG" in url:
+            fn = str(fn) + ".jpg"
+        else:
+            return 0
+
+        try:
+            image_content = requests.get(url, timeout=20).content
+
+        except Exception as e:
+            with open(fn[:-4] + ".txt",'w') as f:
+                f.write("ERROR: " + str(e) + "|" + url)
+            return
+
+        try:
+            image_file = io.BytesIO(image_content)
+            image = Image.open(image_file).convert('RGB')
+            with open(fn, 'wb') as f:
+                image.save(f)
+            with open(fn[:-4] + ".txt",'w') as f:
+                f.write("SUCCESS: " + url)
+            del image
+
+        except Exception as e:
+            #with open(fn[:-4] + ".txt",'w') as f:
+            #    f.write("ERROR: " + str(e) + "|" + url)
+            return
+
+    def Scrape(list_url):
+        with concurrent.futures.ThreadPoolExecutor() as e:
+            for u in list_url:
+                e.submit(Img.Scraper, u)
+
+    def RemoveSmall(folder,size=2500):
+        list_images = [os.path.join(folder,l) for l in os.listdir(folder) if ".png" in l or ".jpg" in l]
+        too_small = [l for l in list_images if int(os.stat(l).st_size) < size]
+        too_small = too_small + [l[:-3]+"txt" for l in too_small]
+        print('removing {} small images'.format(len(too_small)))
+        [os.remove(i) for i in too_small]
+
+class Duplicates():
+    def __init__(images_destination):
+        self.images_destination = images_destination
+
+    def remove(images_destination):
+
+        list_img = os.listdir(images_destination)
+        list_img = [img for img in list_img if ".txt" not in img]
+
+        if len(list_img) > 1:
+            df = pd.DataFrame()
+
+            for img in list_img:
+                try:
+                    im = Image.open(os.path.join(images_destination,img))
+                    width, height = im.size
+                    im = np.array(im)
+                    w,h,d = im.shape
+                    im.shape = (w*h, d)
+                    values_ = tuple(np.average(im, axis=0))
+                    values_ = "_".join([str(int(round(v))) for v in values_])
+                    print(width,height,values_,img)
+                    tmp = pd.DataFrame([width,height,values_,img]).T
+                    tmp.columns = ['w','h','rgb','id']
+                    df = df.append(tmp)
+                except Exception as e:
+                    print(e)
+                    pass
+            df.columns = ['w','h','rgb','id']
+            df['w'] = df['w'].astype(int)
+            df['h'] = df['h'].astype(int)
+            df['iid'] = df['rgb'].astype(str) + df['w'].astype(str) + df['h'].astype(str)
+            dfn = df.drop_duplicates(subset='iid', keep="first")
+
+            print('--- Found {}/{} duplicates'.format(len(df) - len(dfn),len(df)))
+
+            removal_list = [im for im in list(df['id']) if im not in list(dfn['id'])]
+
+            for img in removal_list:
+                os.remove(os.path.join(images_destination,img))
+                os.remove(os.path.join(images_destination,img[:-3] + "txt"))
+        else:
+            print("no image files found, no duplicates removed")
+
+#nlp = spacy.load('en_core_web_sm')
+
+
+###### Parse() helper classes
 class dateparser():
     def __init__(self,url,list_url,filename):
         self.url = url
@@ -398,12 +592,57 @@ class Parse():
     - Texts() parses texts from .html file using boilerpipe
     - Languages() parses languages from webpage content using the langid library
     - Dates() parses dates from urls and webpage content by regex patters and the htmldate library
+    - Entities() parses entities from texts using spacy, requires different spacy models to be installed.
+    - ContextImages() parses other images on page. Warning: can result in a lot(!) of images
     """
 
     def __init__(self,photo, base_path):
         super(Parse, self).__init__()
         self.photo = photo
         self.base_path = base_path
+
+    def Pages(base_path,photo):
+        photo_folder = os.path.join(base_path, photo)
+        num_iterations = len([fol for fol in os.listdir(photo_folder) if os.path.isdir(os.path.join(photo_folder,fol)) and "source" not in fol])
+        start_iter = 1
+        range_iter = [str(i) for i in list(range(1,num_iterations+1))]
+        folder_base = os.path.join(photo_folder,photo)
+
+        for iteration in range_iter:
+
+            # Import page URLs from .json files (using Json.extract_pages())
+            iteration_path = folder_base + "_" + str(iteration)
+            jsFiles = [js for js in os.listdir(iteration_path) if ".json" in js]
+
+            list_urls = []
+            for js in jsFiles:
+                json_file_path = os.path.join(folder_base + "_" + str(iteration), js)
+                list_urls += Json.extract_pages(json_file_path)
+
+            # Import previously scraped URLs if iteration number is larger than 1:
+            scraped_urls = []
+            if int(iteration) > 1:
+                for i in range(1,int(iteration)):
+                    try:
+                        with open(os.path.join(folder_base + "_" + str(i), "html","results.txt"), 'r', encoding='utf-8') as f:
+                            print("INFO: importing from {}".format(os.path.join(folder_base + "_" + str(i), "html","results.txt")))
+                            lu = f.readlines()
+                            lu = [l.split('|') for l in lu]
+                            lu = [l for l in lu if len(l) == 2]
+                            lu = [l[1].replace('\n','') for l in lu]
+                            scraped_urls = scraped_urls + lu
+                    except FileNotFoundError:
+                        print(os.path.join(folder_base + "_" + str(i), "html","results.txt"), "not found")
+
+            #Scrape All Page URLs to 'image[...]/html' folder
+            destination_path = os.path.join(folder_base + "_" + str(iteration), "html")
+            if not os.path.exists(destination_path):
+                os.makedirs(destination_path)
+
+            list_urls = [u for u in list_urls if u not in scraped_urls]
+            print('INFO: Scraping {} URLs in iteration {}'.format(len(list_urls),iteration))
+
+            pagescraper.PoolScrape(list_urls, destination_path)
 
     def Texts(base_path,photo):
         photo_folder = os.path.join(base_path, photo)
